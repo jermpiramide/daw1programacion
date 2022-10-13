43c0477fee23f69eaf05cc4b7c01bfcab52e55d5
jermpiramide <220016@fppiramide.com>
2022-10-13 12:31:11 +0200

    Desde el pc del instituto: actualización U02.

---
+++ src/U03ejemp01.java
@@ -0,0 +1,32 @@
+// Crea un array en el que se almacenen los datos de porcentaje
+// de llenado de un pantano en cada uno de los meses del año.
+// A partir de ese array, muestra por pantalla los correspondientes a los meses de verano.
+// (Crea el array como objeto y también explícitamente)
+
+
+public class U03ejemp01 {
+    public static void main(String args[]) {
+
+        /*
+        double[] lleno;
+
+        lleno = new double[12];
+
+        for(int i=0; i<12; i++){
+            lleno[i] = i*8;
+        }
+
+        for(int i=0; i<12; i++){
+            System.out.printf("%.1f ", lleno[i]);
+        }
+
+         */
+
+        double[] lleno = {78, 76, 45, 76, 56, 34, 54, 54, 23, 54, 23, 12};
+
+        for(int i=6; i<10; i++){
+            System.out.printf("%.1f ", lleno[i]);
+        }
+        System.out.println(lleno);
+    }
+}
---
+++ src/U03ejemp02.java
@@ -0,0 +1,42 @@
+// Crea un array de notas de un alumno. Además crea una variable que almacene el número de años
+// que lleva estudiando en el centro. Genera un método que "cambie" el valor de los años que
+// lleva estudiando en el centro (pasado como argumento). Luego otro que cambie la primera nota del array
+// (pasado como argumento).
+// Invoca los dos métodos, presenta luego los datos y observa lo que pasa. (utiliza .length)
+
+public class U03ejemp02 {
+    public static void main(String args[]){
+
+        double[] nota = {3, 6, 7, 8, 10, 2, 4, 0, 9};
+        int anos = 10, posicion = 0, nuevanota = 8;
+
+        System.out.println("Años: " + anos);
+
+        duplicaEntero(anos);
+
+        System.out.println("La variable \"anos\" después de aplicar el método \"duplicaEntero\" vale: " + anos);
+
+        muestraArray(nota);
+
+        System.out.println();
+
+        modificaArray(nota, posicion, nuevanota);
+    }
+
+    public static void duplicaEntero(int anos){
+
+        anos *= 2;
+        System.out.println("Años duplicados: " + anos);
+    }
+
+    public static void muestraArray(double[] array){
+
+        for(int i=0; i<array.length; i++){
+            System.out.printf("%.1f ", array[i]);
+        }
+    }
+
+    public static void modificaArray(double[] array, int posicion, int cambio);
+
+    array[posicion] = cambio;
+}
