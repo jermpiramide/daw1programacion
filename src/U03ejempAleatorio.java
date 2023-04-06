905ad8759a19307cf7da9dbe5b8110f69e2b2a57
ASUSPro <220016@fppiramide.com>
2023-04-06 11:02:47 +0200

    Desde ASUSPro: actualización.

--- src/U03ejempAleatorio.java
+++ src/U03ejempAleatorio.java
@@ -6,25 +6,26 @@ public class U03ejempAleatorio {
         int numeroint;
         double numerodouble;
 
-        for(int i=0; i<100000; i++){
+        for(int i=0; i<10000; i++){
             numerodouble = Math.random(); //Math.random() devuelve un número en el intervalo [0-1).
             System.out.println("nuerodouble: " + numerodouble);
 
-            numerodouble = (numerodouble*10)+1; //Multiplicamos por 10 para que el intervalo [0-1) pase a ser [0-10).
-            // Y sumamos 1 para que el intervalo [0-10) pase a ser [1-11).
+            numerodouble = (numerodouble*10)+1; //Multiplicamos por 10 para que el intervalo [0-1) pase a ser [0-10)
+            // y sumamos 1 para que el intervalo [0-10) pase a ser [1-11).
             System.out.println("numerodoublex10+1: " + numerodouble);
 
-            numeroint = (int) numerodouble;
+            numeroint = (int) numerodouble; // Casteamos nuberodouble que es de tipo double para que sea de tipo int.
             System.out.println("numeroint: " + numeroint);
 
             if(numeroint<1 || numeroint>10){
+            // Imprime por pantalla numeroint si es menor que 1 o mayor que 10.
                 System.out.println("numeroint es >1 o <10: " + numeroint);
 
                 break;
             }
         }
 
-        for(int i=0; i<100000; i++){
+        for(int i=0; i<10000; i++){
         //Este bucle hace lo mismo que el anterior, pero es más compacto.
             numeroint = (int) (Math.random()*10)+1;
             System.out.println("numero: " + numeroint);
--- src/U03ejerc01.java
+++ src/U03ejerc01.java
@@ -1,5 +1,7 @@
-//Escribe un programa que lea 10 números por teclado y luego los muestre en orden inverso, es decir, el primero que
-// se introduce es el último en mostrarse y viceversa.
+/*
+Escribe un programa que lea 10 números por teclado y luego los muestre en orden inverso, es decir, el primero que
+ se introduce es el último en mostrarse y viceversa.
+*/
 
 import java.util.Scanner;
 
@@ -21,6 +23,7 @@ public class U03ejerc01 {
             System.out.printf("Introduce un número para la posición %d del array: ", i);
             lista[i] = sc.nextDouble();
         }
+        System.out.println();
 
         muestraArray(lista);
         invierteArray(lista);
@@ -32,17 +35,28 @@ public class U03ejerc01 {
         for(int i=0; i<array.length; i++){
             System.out.printf("%.1f ", array[i]);
         }
+
+        System.out.println();
     }
 
     public static void invierteArray(double[] array){
         double aux;
         int size = array.length;
         System.out.println("Tamaño del array: " + size);
+        // System.out.println("size/2: " + (size/2)); // Recordemos que el operador / nos devuelve la parte entera.
 
-        for(int i=0; i<array.length; i++){
+        for(int i=0; i<size/2; i++){
+        // Al estar permutando elementos del array en cada paso se modifican dos posiciones, por lo que no es necesario
+        // hacer tantas iteraciones como posiciones tiene el array.
+        // En el caso de que el tamaño del array sea par, este bucle realiza un número de iteraciones igual a la mitad
+        // de su tamaño.
+        // En el caso de que el tamaño del array sea impar, este bucle realiza un número de iteraciones igual a la mitad
+        // de su tamaño menos uno. Podríamos pensar que esto es un problema porque nos dejamos una posición sin
+        // permutar, pero esto no es un problema porque esa posición vería modificado su valor consigo misma y , por
+        // por tant, no vería modificado su valor.
             aux = array[i];
-            array[i] = array[array.length-1-i];
-            array[array.length-1-i] = aux;
+            array[i] = array[size-1-i];
+            array[size-1-i] = aux;
             //Este bucle se puede optimizar para ahorrar un paso en el caso de que la longitud del array sea impar.
         }
     }
