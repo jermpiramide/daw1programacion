af0f215480618dbe702b4a05e25c48677594be48
ASUSPro <220016@fppiramide.com>
2023-04-07 14:40:06 +0200

    Desde ASUSPro: actualización.

---
+++ src/U03ejerc12.java
@@ -0,0 +1,8 @@
+/*
+Implementa un programa que determine la frecuencia de aparición de cada vocal en una cadena de caracteres introducida
+por teclado. Comenzará solicitando la cadena y, posteriormente, construirá un array con las frecuencias de cada vocal.
+Finalmente, imprimirá el array de frecuencias por pantalla.(NOTA: No debe ser sensible a mayúsculas/minúsculas).
+ */
+
+public class U03ejerc12 {
+}
