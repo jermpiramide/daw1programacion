a0f8a7e06dfe31f6aeec7c5e1d3e58e7f0a686f4
ASUSPro <220016@fppiramide.com>
2023-04-16 19:14:38 +0200

    Desde ASUSPro: actualización.

--- src/U03ejerc12.java
+++ src/U03ejerc12.java
@@ -4,5 +4,60 @@ por teclado. Comenzará solicitando la cadena y, posteriormente, construirá un 
 Finalmente, imprimirá el array de frecuencias por pantalla.(NOTA: No debe ser sensible a mayúsculas/minúsculas).
  */
 
+import java.util.Scanner;
+
 public class U03ejerc12 {
+
+    public static void main(String Args[]){
+
+        String cadena;
+        Scanner sc;
+        char caracter;
+        int a, e, i, o, u;
+
+        a = 0;
+        e = 0;
+        i = 0;
+        o = 0;
+        u = 0;
+
+        sc = new Scanner(System.in);
+
+        System.out.println("Introduce una palabra (sin tildes ni diéresis en las vocales): ");
+        cadena = sc.nextLine();
+
+        for(int j=0; j<cadena.length(); j++){
+            caracter = cadena.charAt(j);
+
+            switch(caracter){
+                case 'a':
+                case 'A':
+                    a++;
+                    break;
+                case 'e':
+                case 'E':
+                    e++;
+                    break;
+                case 'i':
+                case 'I':
+                    i++;
+                    break;
+                case 'o':
+                case 'O':
+                    o ++;
+                    break;
+                case 'u':
+                case 'U':
+                    u++;
+                    break;
+            }
+        }
+
+        System.out.printf("La vocal a aparece %d veces.\n", a);
+        System.out.printf("La vocal e aparece %d veces.\n", e);
+        System.out.printf("La vocal i aparece %d veces.\n", i);
+        System.out.printf("La vocal o aparece %d veces.\n", o);
+        System.out.printf("La vocal u aparece %d veces.\n", u);
+
+    }
 }
